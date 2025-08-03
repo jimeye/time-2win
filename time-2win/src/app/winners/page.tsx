b1ae18037b87de165d2ed53596552b61d4e40038
Jimmy Fellous <jimmyfellous@gmail.com>
2025-08-03 21:55:32 +0200

    Remove problematic pages and fix build errors

--- time-2win/src/app/winners/page.tsx
+++
@@ -1,9 +0,0 @@
-import WinnersSection from '@/components/sections/WinnersSection';
-
-export default function WinnersPage() {
-  return (
-    <main className="min-h-screen">
-      <WinnersSection />
-    </main>
-  );
-} 
